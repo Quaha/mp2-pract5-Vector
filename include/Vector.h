947c9413e92f9a6cf3cb8a12509633a62993d2eb
Quaha <ArtemRomanov2017@yandex.ru>
2024-11-08 21:17:38 +0300

    stack functionality

--- include/Vector.h
+++ include/Vector.h
@@ -1,3 +1,204 @@
 #pragma once
 
-#include <iostream>
+template <typename VectorType> class Vector {
+private:
+
+	VectorType* prev_vector = nullptr; // curr_capacity / 2
+	VectorType* curr_vector = nullptr; // curr_capacity
+	VectorType* next_vector = nullptr; // curr_capacity * 2
+
+	int curr_capacity = 2;
+
+	int prev_sz = 0;
+	int curr_sz = 1; // there is a fake element in order to simplify the implementation
+	int next_sz = 0;
+
+	void destruct(Vector* vector) {
+		delete[] vector->prev_vector;
+		delete[] vector->curr_vector;
+		delete[] vector->next_vector;
+	}
+
+	void construct(Vector* vector) {
+		vector->prev_vector = new VectorType[1]();
+		vector->curr_vector = new VectorType[2]();
+		vector->next_vector = new VectorType[4]();
+
+		vector->curr_sz = 1;
+		vector->curr_capacity = 2;
+		vector->prev_sz = 0;
+		vector->next_sz = 0;
+	}
+
+public:
+
+	Vector() {
+		construct(this);
+	}
+
+	~Vector() {
+		destruct(this);
+	}
+
+	Vector(const Vector& other) {
+		this->curr_capacity = other.curr_capacity;
+
+		this->prev_sz = other.prev_sz;
+		this->curr_sz = other.curr_sz;
+		this->next_sz = other.next_sz;
+
+		this->prev_vector = new VectorType[prev_sz];
+		this->curr_vector = new VectorType[curr_sz];
+		this->next_vector = new VectorType[next_sz];
+
+		for (int i = 0; i < prev_sz; ++i) {
+			this->prev_vector[i] = other.prev_vector[i];
+		}
+
+		for (int i = 0; i < curr_sz; ++i) {
+			this->curr_vector[i] = other.curr_vector[i];
+		}
+
+		for (int i = 0; i < next_sz; ++i) {
+			this->next_vector[i] = other.next_vector[i];
+		}
+	}
+
+	Vector(Vector&& other) {
+		this->prev_vector = other.prev_vector;
+		this->curr_vector = other.curr_vector;
+		this->next_vector = other.next_vector;
+
+		this->curr_capacity = other.curr_capacity;
+
+		this->curr_sz = other.curr_sz;
+		this->prev_sz = other.prev_sz;
+		this->next_sz = other.next_sz;
+
+		construct(&other);
+	}
+
+	Vector& operator=(const Vector& other) {
+		if (this == &other) return *this;
+
+		destruct(this);
+
+		this->curr_capacity = other.curr_capacity;
+
+		this->prev_sz = other.prev_sz;
+		this->curr_sz = other.curr_sz;
+		this->next_sz = other.next_sz;
+
+		this->prev_vector = new VectorType[prev_sz];
+		this->curr_vector = new VectorType[curr_sz];
+		this->next_vector = new VectorType[next_sz];
+
+
+		for (int i = 0; i < prev_sz; ++i) {
+			this->prev_vector[i] = other.prev_vector[i];
+		}
+
+		for (int i = 0; i < curr_sz; ++i) {
+			this->curr_vector[i] = other.curr_vector[i];
+		}
+
+		for (int i = 0; i < next_sz; ++i) {
+			this->next_vector[i] = other.next_vector[i];
+		}
+
+		return *this;
+	}
+
+	Vector& operator=(Vector&& other) {
+		if (this == &other) return *this;
+
+		destruct(this);
+
+		this->prev_vector = other.prev_vector;
+		this->curr_vector = other.curr_vector;
+		this->next_vector = other.next_vector;
+
+		this->curr_capacity = other.curr_capacity;
+
+		this->curr_sz = other.curr_sz;
+		this->prev_sz = other.prev_sz;
+		this->next_sz = other.next_sz;
+
+		construct(&other);
+
+		return *this;
+	}
+
+	void push_back(const VectorType& value) {
+		curr_vector[curr_sz] = value;
+		++curr_sz;
+
+		if (prev_sz == 0) {
+			next_vector[next_sz] = curr_vector[next_sz];
+			next_vector[next_sz + 1] = curr_vector[next_sz + 1];
+			next_sz += 2;
+		}
+		else {
+			--prev_sz;
+		}
+
+
+		if (curr_sz == curr_capacity) {
+			delete[] prev_vector;
+
+			prev_vector = curr_vector;
+			curr_vector = next_vector;
+
+			curr_capacity *= 2;
+			next_vector = new VectorType[curr_capacity * 2];
+
+			prev_sz = 0;
+			next_sz = 0;
+		}
+	}
+
+	void pop_back() {
+		if (empty()) {
+			throw std::out_of_range("It is forbidden to use the method .pop_back() on an empty vector!");
+		}
+
+		--curr_sz;
+
+		if (next_sz != 0) {
+			next_sz -= 2;
+		}
+		else {
+			prev_vector[prev_sz] = curr_vector[prev_sz];
+			++prev_sz;
+		}
+
+		if (curr_sz == curr_capacity / 4) {
+			delete[] next_vector;
+
+			next_vector = curr_vector;
+			curr_vector = prev_vector;
+
+			curr_capacity /= 2;
+
+			prev_vector = new VectorType[curr_capacity / 2];
+
+			prev_sz = 0;
+			next_sz = 0;
+		}
+	}
+
+	VectorType back() const {
+		if (empty()) {
+			throw std::out_of_range("It is forbidden to use the method .back() on an empty vector!");
+		}
+		return curr_vector[curr_sz - 1];
+	}
+
+	size_t size() const {
+		return curr_sz - 1;
+	}
+
+	bool empty() const {
+		return curr_sz == 1;
+	}
+};
--- test/test_Vector.cpp
+++ test/test_Vector.cpp
@@ -1,7 +1,238 @@
+#include "Vector.h"
+
 #include <gtest.h>
+#include <vector>
+#include <random>
 
-TEST(NameOfTheTestedClass, just_an_empty_test) {
+TEST(Vector, can_create_Vector) {
+    ASSERT_NO_THROW(Vector<int> Vector);
+}
 
-	EXPECT_EQ(1, 1);
+TEST(Vector, default_constructor) {
+    Vector<int> Vector;
+    EXPECT_TRUE(Vector.empty());
+    EXPECT_EQ(Vector.size(), 0);
+}
 
+TEST(Vector, correct_push_back_working) {
+    Vector<int> Vector;
+
+    for (int i = 0; i < 100; i++) {
+        Vector.push_back(i);
+        EXPECT_EQ(Vector.back(), i);
+    }
+}
+
+TEST(Vector, push_back_and_size) {
+    Vector<int> Vector;
+    Vector.push_back(5);
+    Vector.push_back(10);
+
+    EXPECT_FALSE(Vector.empty());
+    EXPECT_EQ(Vector.size(), 2);
+}
+
+TEST(Vector, correct_pop_back_working) {
+    Vector<int> Vector;
+
+    for (int i = 0; i < 100; i++) {
+        Vector.push_back(i);
+    }
+
+    for (int i = 99; i >= 0; i--) {
+        EXPECT_EQ(Vector.back(), i);
+        Vector.pop_back();
+    }
+}
+
+TEST(Vector, pop_back_and_size) {
+    Vector<int> Vector;
+    Vector.push_back(5);
+    Vector.push_back(10);
+    Vector.pop_back();
+
+    EXPECT_EQ(Vector.size(), 1);
+    EXPECT_EQ(Vector.back(), 5);
+}
+
+
+TEST(Vector, back_on_empty_Vector) {
+    Vector<int> Vector;
+    EXPECT_ANY_THROW(Vector.back());
+}
+
+TEST(Vector, pop_back_on_empty_Vector) {
+    Vector<int> Vector;
+    EXPECT_ANY_THROW(Vector.pop_back());
+}
+
+TEST(Vector, copy_constructor) {
+    Vector<int> Vector1;
+    Vector1.push_back(1);
+    Vector1.push_back(2);
+
+    Vector<int> Vector2(Vector1);
+
+    EXPECT_EQ(Vector2.size(), Vector1.size());
+    EXPECT_EQ(Vector2.back(), Vector1.back());
+
+    Vector2.pop_back();
+    EXPECT_NE(Vector2.size(), Vector1.size());
+}
+
+TEST(Vector, copy_assignment_operator1) {
+    Vector<int> Vector1;
+    Vector1.push_back(5);
+    Vector1.push_back(10);
+
+    Vector<int> Vector2;
+    Vector2 = Vector1;
+
+    EXPECT_EQ(Vector2.size(), Vector1.size());
+    EXPECT_EQ(Vector2.back(), Vector1.back());
+
+    Vector2.pop_back();
+    EXPECT_NE(Vector2.size(), Vector1.size());
+}
+
+TEST(Vector, copy_assignment_operator2) {
+    Vector<int> Vector1;
+    Vector1.push_back(5);
+
+    ASSERT_NO_THROW(Vector1 = Vector1);
+}
+
+TEST(Vector, move_constructor) {
+    Vector<int> Vector1;
+    Vector1.push_back(8);
+    Vector1.push_back(16);
+
+    Vector<int> Vector2(std::move(Vector1));
+
+    EXPECT_EQ(Vector2.size(), 2);
+    EXPECT_EQ(Vector2.back(), 16);
+    EXPECT_TRUE(Vector1.empty());
+}
+
+TEST(Vector, move_assignment_operator1) {
+    Vector<int> Vector1;
+    Vector1.push_back(8);
+    Vector1.push_back(16);
+
+    Vector<int> Vector2;
+    Vector2 = std::move(Vector1);
+
+    EXPECT_EQ(Vector2.size(), 2);
+    EXPECT_EQ(Vector2.back(), 16);
+    EXPECT_TRUE(Vector1.empty());
+}
+
+TEST(Vector, move_assignment_operator2) {
+    Vector<int> Vector1;
+    Vector1.push_back(5);
+
+    ASSERT_NO_THROW(Vector1 = std::move(Vector1));
+}
+
+TEST(Vector, large_number_of_elements) {
+    Vector<int> Vector;
+    for (int i = 0; i < 1000; ++i) {
+        Vector.push_back(i);
+    }
+
+    EXPECT_EQ(Vector.size(), 1000);
+    EXPECT_EQ(Vector.back(), 999);
+}
+
+TEST(Vector, push_and_pop_multiple_times) {
+    Vector<int> Vector;
+    for (int i = 0; i < 5; ++i) {
+        Vector.push_back(i);
+    }
+    for (int i = 0; i < 5; ++i) {
+        Vector.pop_back();
+    }
+
+    EXPECT_TRUE(Vector.empty());
+    EXPECT_EQ(Vector.size(), 0);
+}
+
+TEST(Vector, empty_after_push_and_pop) {
+    Vector<int> Vector;
+    Vector.push_back(1);
+    Vector.pop_back();
+
+    EXPECT_TRUE(Vector.empty());
+}
+
+TEST(Vector, combination_of_push_and_pop_1) {
+    Vector<int> Vector;
+
+    for (int i = 0; i < 100; i++) {
+        Vector.push_back(i);
+    }
+
+    for (int i = 0; i < 100; i++) {
+        Vector.pop_back();
+    }
+
+    for (int i = 0; i < 100; i++) {
+        Vector.push_back(i);
+        EXPECT_EQ(Vector.back(), i);
+    }
+
+    for (int i = 99; i >= 0; i--) {
+        EXPECT_EQ(Vector.back(), i);
+        Vector.pop_back();
+    }
+}
+
+TEST(Vector, combination_of_push_and_pop_2) {
+    Vector<int> Vector;
+
+    for (int i = 0; i < 16; i++) {
+        ASSERT_NO_THROW(Vector.push_back(i));
+    }
+
+    ASSERT_NO_THROW(Vector.pop_back());
+    for (int i = 0; i < 20; i++) {
+        ASSERT_NO_THROW(Vector.push_back(i));
+    }
+
+    while (!Vector.empty()) {
+        ASSERT_NO_THROW(Vector.pop_back());
+    }
+}
+
+TEST(Vector, stress_test_push_and_pop) {
+    std::srand(time(0));
+
+    int COUNT_OF_TESTS = 1000;
+    int ACTIONS_PER_TEST = 10000;
+
+    for (int CURR_TEST_CASE = 0; CURR_TEST_CASE < COUNT_OF_TESTS; CURR_TEST_CASE++) {
+        Vector<int> Vector;
+        std::vector<int> Vector_copy;
+
+        for (int CURR_ACTION = 0; CURR_ACTION < ACTIONS_PER_TEST; CURR_ACTION++) {
+            int type = rand() % 2;
+
+            if (type == 0) {
+                int value = rand();
+                Vector.push_back(value);
+                Vector_copy.push_back(value);
+            }
+            if (type == 1) {
+                if (!Vector.empty()) {
+                    Vector.pop_back();
+                    Vector_copy.pop_back();
+                }
+            }
+
+            if (!Vector.empty()) {
+                EXPECT_EQ(Vector.back(), Vector_copy.back());
+            }
+            EXPECT_EQ(Vector.size(), Vector_copy.size());
+        }
+    }
 }
